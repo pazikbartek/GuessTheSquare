dbe17d882e732fb928ec7b7c6b6cf922d9908f18
bartek <bartek.pazik@interia.pl>
2019-01-23 14:33:34 +0100

    init

---
+++ square.js
@@ -0,0 +1,7 @@
+class Square{
+    constructor(){
+        this.squares = ["fas fa-heart", "fas fa-heart", "fas fa-apple-alt", "fas fa-apple-alt", "fas fa-diamond",
+        "fas fa-diamond", "fas fa-gem", "fas fa-gem", "fas fa-sun", "fas fa-sun", "far fa-futbol", "far fa-futbol",
+        "fas fa-moon", "fas fa-moon", "fas fa-triangle", "fas fa-triangle"]
+    }
+}
